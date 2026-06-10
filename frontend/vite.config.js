1ec4e55f269d5db1faab782f32b1af103c729600
dharneesh2007 <dharneeshd007@gmail.com>
2026-06-10 14:15:27 +0530

    Update base URL and homepage for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Patient-Monitoring/',
+  base: '/AI-PATIENTS-HEALTH-MANAGEMENT/',
   server: {
     proxy: {
       '/api': {
